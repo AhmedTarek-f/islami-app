815fec443111a3051903deab62ed7daba0f3838f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-12 13:36:30 +0200

    adding app icons

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,18 @@
+abstract final class AppIcons {
+
+  static const hadithActiveIcon = "assets/icons/hadith_active_icon.svg";
+  static const hadithInactiveIcon = "assets/icons/hadith_inactive_icon.svg";
+  static const pauseIcon = "assets/icons/pause_icon.svg";
+  static const playIcon = "assets/icons/play_icon.svg";
+  static const quranActiveIcon = "assets/icons/quran_active_icon.svg";
+  static const radioActiveIcon = "assets/icons/radio_active_icon.svg";
+  static const radioInactiveIcon = "assets/icons/radio_inactive_icon.svg";
+  static const searchBookIcon = "assets/icons/search_book_icon.svg";
+  static const sebhaActiveIcon = "assets/icons/sebha_active_icon.svg";
+  static const sebhaInactiveIcon = "assets/icons/sebha_inactive_icon.svg";
+  static const soundOffIcon = "assets/icons/sound_off_icon.svg";
+  static const soundOnIcon = "assets/icons/sound_on_icon.svg";
+  static const timeActiveIcon = "assets/icons/time_active_icon.svg";
+  static const timeInactiveIcon = "assets/icons/time_inactive_icon.svg";
+
+}
