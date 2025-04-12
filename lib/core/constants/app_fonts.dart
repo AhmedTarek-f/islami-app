a56cfbf78c6cb31d96293ae9e42958e89a3010d5
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-12 13:57:11 +0200

    adding app fonts

---
+++ lib/core/constants/app_fonts.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/core/constants/app_colors.dart';
+
+abstract final class AppFonts {
+  static const TextStyle fontSize36Bold = TextStyle(
+      fontSize: 36,
+      fontWeight: FontWeight.bold,
+      color: AppColors.white
+  );
+  static const TextStyle fontSize32Bold = TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.bold,
+      color: AppColors.white
+  );
+  static const TextStyle fontSize24Bold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primary
+  );
+  static const TextStyle fontSize20Bold = TextStyle(
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: AppColors.primary
+  );
+  static const TextStyle fontSize16Bold = TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.bold,
+      color: AppColors.primary
+  );
+  static const TextStyle fontSize14Bold = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
+      color: AppColors.black
+  );
+  static const TextStyle fontSize12Bold = TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.bold,
+      color: AppColors.white
+  );
+}
