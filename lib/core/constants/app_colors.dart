49559bd707cbe2e0f6796159efe49ba218a21960
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-12 13:52:39 +0200

    adding new colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,4 +4,7 @@ abstract final class AppColors{
   static const Color primary = Color(0xffE2BE7F);
   static const Color black = Color(0xff212121);
   static const Color white = Color(0xffFFFFFF);
+  static const Color offWhite = Color(0xffFEFFE8);
+  static const Color gray = Color(0xff707070);
+  static const Color brown = Color(0xff946107);
 }
