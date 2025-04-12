a4e49127ea2061bf95f79ba0f3a7d307999a4aef
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-12 13:37:01 +0200

    adding app images

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,24 @@
+abstract final class AppImages{
+  // Images
+  static const String hadithBorderLeft = "assets/images/hadith_border_left.png";
+  static const String hadithBorderRight = "assets/images/hadith_border_right.png";
+  static const String mostRecentlySuraImage = "assets/images/most_recently_sura_image.png";
+  static const String onBoarding1 = "assets/images/on_boarding_1.png";
+  static const String onBoarding2 = "assets/images/on_boarding_2.png";
+  static const String onBoarding3 = "assets/images/on_boarding_3.png";
+  static const String onBoarding4 = "assets/images/on_boarding_4.png";
+  static const String onBoarding5 = "assets/images/on_boarding_5.png";
+  static const String quranScreenBackground = "assets/images/quran_screen_background.png";
+  static const String radioActiveBackground = "assets/images/radio_active_background.png";
+  static const String radioInactiveBackground = "assets/images/radio_inactive_background.png";
+  static const String radioScreenBackground = "assets/images/radio_screen_background.png";
+  static const String sebhaScreenBackground = "assets/images/sebha_screen_background.png";
+  static const String suraDetailsBorderLeft = "assets/images/sura_details_border_left.png";
+  static const String suraDetailsBorderRight = "assets/images/sura_details_border_right.png";
+  static const String suraDetailsScreenFooterImage = "assets/images/sura_details_screen_footer_image.png";
+  static const String suraNumberImg = "assets/images/sura_number_img.png";
+  static const String timeScreenBackground = "assets/images/time_screen_background.png";
+
+  // logos
+  static const String islamiMosqueLogo = "assets/logos/islami_mosque_logo.png";
+}
