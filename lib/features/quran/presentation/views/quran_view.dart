4aae3dcb9cda5da8e59a6fb26bb01b384004639a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 22:59:22 +0200

    adding quran view

---
+++ lib/features/quran/presentation/views/quran_view.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/core/constants/app_images.dart';
+import 'package:islami_app/features/quran/presentation/views/widgets/quran_view_body.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
+
+class QuranView extends StatelessWidget {
+  const QuranView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(
+                AppImages.quranScreenBackground,
+            ),
+          fit: BoxFit.cover
+        )
+      ),
+      child: BlocProvider<QuranCubit>(
+        create: (context) => QuranCubit(),
+        child: const SafeArea(
+            child: QuranViewBody()
+        ),
+      ),
+    );
+  }
+}
