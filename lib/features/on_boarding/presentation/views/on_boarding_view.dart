50628fa03179bfa7b0d704e41048ecd3f4b47e4f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 22:56:11 +0200

    adding the onBoardingCubit provider at main

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:islami_app/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart';
-import 'package:islami_app/features/on_boarding/presentation/views_model/on_boarding_cubit.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocProvider<OnBoardingCubit>(
-          create: (context) => OnBoardingCubit(),
-          child: const SafeArea(child: OnBoardingViewBody())
-      ),
+    return const Scaffold(
+      body: SafeArea(child: OnBoardingViewBody()),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:islami_app/features/on_boarding/presentation/views_model/on_boarding_cubit.dart';
 import 'package:islami_app/islami_app.dart';
 
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
 
-  runApp(const IslamiApp());
+  runApp(
+      BlocProvider<OnBoardingCubit>(
+          create: (context) => OnBoardingCubit(),
+          child: const IslamiApp()
+      )
+  );
 }
