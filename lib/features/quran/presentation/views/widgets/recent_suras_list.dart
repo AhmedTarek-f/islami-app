3c735029d36081918f9b0184f116ebacb6ffc592
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 23:00:33 +0200

    adding recent suras list

---
+++ lib/features/quran/presentation/views/widgets/recent_suras_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/features/quran/presentation/views/widgets/recent_sura_item.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
+
+class RecentSurasList extends StatelessWidget {
+  const RecentSurasList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = BlocProvider.of<QuranCubit>(context);
+    return SizedBox(
+      height: 150.h,
+      child: ListView.separated(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (_, index) => RecentSuraItem(suraData: controller.surasList[index]),
+          separatorBuilder: (context, index) => SizedBox(width: 10.w,),
+          itemCount: controller.surasList.length
+      ),
+    );
+  }
+}
