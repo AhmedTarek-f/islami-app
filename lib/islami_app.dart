649ff989a99017cf982a2297fa7fe4e305becedd
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 23:01:46 +0200

    using screen redirect function

--- lib/islami_app.dart
+++ lib/islami_app.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islami_app/core/constants/app_theme.dart';
-import 'package:islami_app/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:islami_app/features/on_boarding/presentation/views_model/on_boarding_cubit.dart';
 
 class IslamiApp extends StatelessWidget {
   const IslamiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final onBoardingController = BlocProvider.of<OnBoardingCubit>(context);
     return ScreenUtilInit(
       designSize: const Size(430, 932),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Islami',
         theme: AppTheme.lightMode,
-        home: const OnBoardingView(),
+        home: onBoardingController.screenRedirect(),
       ),
     );
   }
