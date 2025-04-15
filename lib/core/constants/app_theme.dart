89f992a1cb70fd16ed3dce5a3094c6930bc3a48b
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 22:50:48 +0200

    adding bottomNavigationBarTheme

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/core/constants/app_colors.dart';
+import 'package:islami_app/core/constants/app_fonts.dart';
 
 abstract final class AppTheme{
   static ThemeData lightMode = ThemeData(
     useMaterial3: true,
     scaffoldBackgroundColor: AppColors.black,
     fontFamily: "Janna LT",
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      backgroundColor: AppColors.primary,
+      selectedItemColor: AppColors.white,
+      unselectedItemColor: AppColors.black,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+      showUnselectedLabels: false,
+      selectedLabelStyle: AppFonts.fontSize12Bold,
+    )
   );
 }
