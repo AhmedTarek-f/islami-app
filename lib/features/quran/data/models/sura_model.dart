be1e434c76e036b724de37cd04bcc87803004d51
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 22:58:33 +0200

    adding initial sura model class

---
+++ lib/features/quran/data/models/sura_model.dart
@@ -0,0 +1,12 @@
+class SuraModel{
+  const SuraModel({
+    required this.suraNumber,
+    required this.suraNameEn,
+    required this.suraNameAr,
+    required this.versesNumber,
+  });
+  final String suraNumber;
+  final String suraNameEn;
+  final String suraNameAr;
+  final String versesNumber;
+}
