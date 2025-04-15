0ea0c833e61837bd5404210852a0603b4a66cc54
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-15 22:56:38 +0200

    using the IslamiLogo widget

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:islami_app/core/constants/app_images.dart';
+import 'package:islami_app/core/common_widgets/islami_logo.dart';
 import 'package:islami_app/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 import 'package:islami_app/features/on_boarding/presentation/views/widgets/on_boarding_page_view_footer.dart';
 
@@ -13,12 +13,7 @@ class OnBoardingViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: Column(
         children: [
-          RPadding(
-            padding: const EdgeInsets.symmetric(horizontal: 69.5),
-              child: Image.asset(
-                  AppImages.islamiMosqueLogo
-              ),
-          ),
+          IslamiLogo(logoHeight: 171.h,logoWidth: 291.w,),
           const Expanded(child: OnBoardingPageView()),
           const OnBoardingPageViewFooter(),
         ],
