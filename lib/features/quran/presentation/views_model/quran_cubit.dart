2ae0d5e9f772c39accb9a492a7c591fba3cb226f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-16 14:31:51 +0200

    adding load sura data function

--- lib/features/quran/presentation/views_model/quran_cubit.dart
+++ lib/features/quran/presentation/views_model/quran_cubit.dart
@@ -1,51 +1,22 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:islami_app/features/quran/data/models/sura_model.dart';
 import 'package:islami_app/features/quran/presentation/views_model/quran_state.dart';
 
 class QuranCubit extends Cubit<QuranState> {
   QuranCubit() : super(QuranInitial());
-  final List<SuraModel> surasList = const [
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-    SuraModel(
-        suraNumber: "1",
-        suraNameEn: "El-fatha",
-        suraNameAr: "الفاتحه",
-        versesNumber: "7"
-    ),
-  ];
+  bool isLoading = false;
+  Future<String> loadSuraData({required int suraNumber}) async{
+    try{
+      isLoading = true;
+      emit(LoadSuraLoadingState());
+      final String suraData = await rootBundle.loadString("lib/core/constants/app_data/suras_data/$suraNumber.txt");
+      isLoading = false;
+      emit(LoadSuraSuccessState());
+      return suraData;
+    }
+    catch(e){
+      emit(LoadSuraFailureState(failureMessage: "An error hass ocurred while loading sura: ${e.toString()}"));
+      return "";
+    }
+  }
 }
