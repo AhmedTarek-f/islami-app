e0a10d6cc2cc6c3387162a10a0f44399f757d09e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-16 14:31:34 +0200

    adding load sura states

--- lib/features/quran/presentation/views_model/quran_state.dart
+++ lib/features/quran/presentation/views_model/quran_state.dart
@@ -1,3 +1,11 @@
-sealed class QuranState {}
+sealed class QuranState {
+  const QuranState();
+}
 
 final class QuranInitial extends QuranState {}
+final class LoadSuraSuccessState extends QuranState {}
+final class LoadSuraLoadingState extends QuranState {}
+final class LoadSuraFailureState extends QuranState {
+  const LoadSuraFailureState({required this.failureMessage});
+  final String failureMessage;
+}
