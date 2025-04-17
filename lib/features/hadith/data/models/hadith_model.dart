6c3b5241d22a9d383279a96f8d9709063fcd920a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 16:44:50 +0200

    adding hadith model

---
+++ lib/features/hadith/data/models/hadith_model.dart
@@ -0,0 +1,10 @@
+class HadithModel{
+  HadithModel({
+    required this.hadithNameAr,
+    required this.hadithNameEn,
+    this.hadithContent
+  });
+  final String hadithNameAr;
+  final String hadithNameEn;
+  String? hadithContent;
+}
