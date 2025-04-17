4615073c9767b7da89afdce9fce48b061b0c523a
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:27:21 +0200

    adding logic to search field

--- lib/features/quran/presentation/views/widgets/sura_search_field.dart
+++ lib/features/quran/presentation/views/widgets/sura_search_field.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:islami_app/core/constants/app_colors.dart';
 import 'package:islami_app/core/constants/app_fonts.dart';
 import 'package:islami_app/core/constants/app_icons.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
 
 class SuraSearchField extends StatelessWidget {
   const SuraSearchField({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = BlocProvider.of<QuranCubit>(context);
     return TextField(
+      keyboardType: TextInputType.text,
+      style: AppFonts.fontSize16Bold.copyWith(color: AppColors.white),
       decoration: InputDecoration(
           enabledBorder: buildOutlineInputBorder(),
           focusedBorder: buildOutlineInputBorder(),
@@ -22,6 +27,7 @@ class SuraSearchField extends StatelessWidget {
           hintText: "Sura Name",
           hintStyle: AppFonts.fontSize16Bold.copyWith(color: AppColors.white)
       ),
+      onChanged: (value) => controller.suraSearch(suraName: value.toLowerCase()),
     );
   }
 
