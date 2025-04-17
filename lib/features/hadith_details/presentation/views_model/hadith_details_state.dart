db21a889b1f2343f7b6d69c6d88d4d3b4fd1ffea
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:21:25 +0200

    adding hadith details state

---
+++ lib/features/hadith_details/presentation/views_model/hadith_details_state.dart
@@ -0,0 +1,10 @@
+// sealed class HadithDetailsState {
+//   const HadithDetailsState();
+// }
+// final class HadithDetailsInitial extends HadithDetailsState {}
+// final class LoadHadithSuccessState extends HadithDetailsState {}
+// final class LoadHadithLoadingState extends HadithDetailsState {}
+// final class LoadHadithFailureState extends HadithDetailsState {
+//   const LoadHadithFailureState({required this.failureMessage});
+//   final String failureMessage;
+// }
