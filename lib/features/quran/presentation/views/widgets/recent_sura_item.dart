92d2b6343b53e389c02a78324b4b3837b7310d44
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:31:26 +0200

    adding on tap function to open quran details view

--- lib/features/quran/presentation/views/widgets/recent_sura_item.dart
+++ lib/features/quran/presentation/views/widgets/recent_sura_item.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islami_app/core/constants/app_colors.dart';
 import 'package:islami_app/core/constants/app_fonts.dart';
 import 'package:islami_app/core/constants/app_images.dart';
+import 'package:islami_app/core/utils/loaders.dart';
 import 'package:islami_app/features/quran/data/models/sura_model.dart';
+import 'package:islami_app/features/quran_details/presentation/views/quran_details_view.dart';
+import 'package:islami_app/features/quran_details/presentation/views_model/quran_details_cubit.dart';
 
 class RecentSuraItem extends StatelessWidget {
   const RecentSuraItem({super.key, required this.suraData});
@@ -18,39 +22,51 @@ class RecentSuraItem extends StatelessWidget {
         color: AppColors.primary,
         borderRadius: BorderRadius.circular(20.r),
       ),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.only(left: 8.w),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    suraData.suraNameEn,
-                    style: AppFonts.fontSize24Bold.copyWith(color: AppColors.black),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    suraData.suraNameAr,
-                    style: AppFonts.fontSize24Bold.copyWith(color: AppColors.black),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  Text(
-                    "${suraData.versesNumber} Verses",
-                    style: AppFonts.fontSize14Bold,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
+      child: GestureDetector(
+        onTap: (){
+          Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => BlocProvider<QuranDetailsCubit>(
+                    create: (context) => QuranDetailsCubit(suraNumber: suraData.suraNumber),
+                    child: QuranDetailsView(suraData: suraData),
+                ),
+              ),
+          );
+        },
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 8.w),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      suraData.suraNameEn,
+                      style: AppFonts.fontSize24Bold.copyWith(color: AppColors.black),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      suraData.suraNameAr,
+                      style: AppFonts.fontSize24Bold.copyWith(color: AppColors.black),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      "${suraData.versesNumber} Verses",
+                      style: AppFonts.fontSize14Bold,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Image.asset(
-            AppImages.mostRecentlySuraImage,
-            width: 151.w,
-          )
-        ],
+            Image.asset(
+              AppImages.mostRecentlySuraImage,
+              width: 151.w,
+            )
+          ],
+        ),
       ),
     );
   }
