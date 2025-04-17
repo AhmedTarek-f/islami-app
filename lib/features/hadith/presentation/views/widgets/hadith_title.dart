be8dca80f15062213e6ec1d2cbe0e91de10cc22e
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 16:43:59 +0200

    adding hadith title

---
+++ lib/features/hadith/presentation/views/widgets/hadith_title.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/core/constants/app_colors.dart';
+import 'package:islami_app/core/constants/app_fonts.dart';
+import 'package:islami_app/core/constants/app_images.dart';
+
+class HadithTitle extends StatelessWidget {
+  const HadithTitle({
+    super.key, required this.hadithTitleAr,
+  });
+  final String hadithTitleAr;
+  @override
+  Widget build(BuildContext context) {
+    return RPadding(
+      padding: const EdgeInsets.only(top: 9.0,right: 9,left: 9),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            child: Image.asset(
+              AppImages.hadithBorderLeft,
+              width: 93.w,
+              height: 100.h,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: FittedBox(
+              child: Text(
+                hadithTitleAr,
+                style: AppFonts.fontSize24Bold.copyWith(color: AppColors.black),
+                textDirection: TextDirection.rtl,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Flexible(
+            child: Image.asset(
+              AppImages.hadithBorderRight,
+              width: 93.w,
+              height: 100.h,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
