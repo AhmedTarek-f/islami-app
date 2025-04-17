9abf0550b7884cd4b7e8abcc276ce387419ea401
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:33:37 +0200

    adding sebha design and logic

--- lib/features/sebha/presentation/views/widgets/sebha_view_body.dart
+++ lib/features/sebha/presentation/views/widgets/sebha_view_body.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/core/common_widgets/islami_logo.dart';
+import 'package:islami_app/core/constants/app_fonts.dart';
+import 'package:islami_app/features/sebha/presentation/views/widgets/sebha_content.dart';
 
 class SebhaViewBody extends StatelessWidget {
   const SebhaViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
 
+    return Column(
+      children: [
+        const IslamiLogo(),
+        SizedBox(height: 16.h,),
+        const Text(
+            "سَبِّحِ اسْمَ رَبِّكَ الأعلى ",
+          textDirection: TextDirection.rtl,
+          textAlign: TextAlign.center,
+          style: AppFonts.fontSize36Bold,
+        ),
+        SizedBox(height: 16.h,),
+        const SebhaContent()
       ],
     );
   }
