b111f9262199620a0fdda0625e0b8c95e92d3f12
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:28:13 +0200

    adding on tap function to open quran details view

--- lib/features/quran/presentation/views/widgets/sura_item.dart
+++ lib/features/quran/presentation/views/widgets/sura_item.dart
@@ -1,45 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islami_app/core/constants/app_colors.dart';
 import 'package:islami_app/core/constants/app_fonts.dart';
 import 'package:islami_app/features/quran/data/models/sura_model.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/sura_number.dart';
+import 'package:islami_app/features/quran_details/presentation/views/quran_details_view.dart';
+import 'package:islami_app/features/quran_details/presentation/views_model/quran_details_cubit.dart';
 
 class SuraItem extends StatelessWidget {
   const SuraItem({super.key, required this.suraData});
   final SuraModel suraData;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SuraNumber(suraNumber: suraData.suraNumber,),
-        SizedBox(width: 24.w,),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                suraData.suraNameEn,
-                style: AppFonts.fontSize20Bold.copyWith(color: AppColors.white),
-                overflow: TextOverflow.ellipsis,
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => BlocProvider<QuranDetailsCubit>(
+                create: (context) => QuranDetailsCubit(suraNumber: suraData.suraNumber),
+                child: QuranDetailsView(
+                    suraData: suraData
+                ),
               ),
-              Text(
-                "${suraData.versesNumber} Verses",
-                style: AppFonts.fontSize14Bold.copyWith(color: AppColors.white),
-                overflow: TextOverflow.ellipsis,
-              )
-            ],
+            ),
+        );
+      },
+      child: Row(
+        children: [
+          SuraNumber(suraNumber: suraData.suraNumber,),
+          SizedBox(width: 24.w,),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  suraData.suraNameEn,
+                  style: AppFonts.fontSize20Bold.copyWith(color: AppColors.white),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  "${suraData.versesNumber} Verses",
+                  style: AppFonts.fontSize14Bold.copyWith(color: AppColors.white),
+                  overflow: TextOverflow.ellipsis,
+                )
+              ],
+            ),
           ),
-        ),
-        RPadding(
-          padding: const EdgeInsets.only(right: 3.0),
-          child: Text(
-            suraData.suraNameAr,
-            style: AppFonts.fontSize20Bold.copyWith(color: AppColors.white),
+          RPadding(
+            padding: const EdgeInsets.only(right: 3.0),
+            child: Text(
+              suraData.suraNameAr,
+              style: AppFonts.fontSize20Bold.copyWith(color: AppColors.white),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
