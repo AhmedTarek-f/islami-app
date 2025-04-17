cfc20c2e34a7087e13acb8db7217bc484beed8f7
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 16:43:17 +0200

    adding hadith state

---
+++ lib/features/hadith/presentation/views_model/hadith_state.dart
@@ -0,0 +1,11 @@
+sealed class HadithState {
+  const HadithState();
+}
+
+final class HadithInitial extends HadithState {}
+final class LoadHadithSuccessState extends HadithState {}
+final class LoadHadithLoadingState extends HadithState {}
+final class LoadHadithFailureState extends HadithState {
+  const LoadHadithFailureState({required this.failureMessage});
+  final String failureMessage;
+}
