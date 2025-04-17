bfe906d5d0d139c3b53a707b2b591ab111dd6c6d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-17 17:25:39 +0200

    adding suras search list

---
+++ lib/features/quran/presentation/views/widgets/suras_search_list.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:islami_app/features/quran/presentation/views/widgets/sura_divider.dart';
+import 'package:islami_app/features/quran/presentation/views/widgets/sura_item.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_state.dart';
+
+class SurasSearchList extends StatelessWidget {
+  const SurasSearchList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = BlocProvider.of<QuranCubit>(context);
+    return BlocBuilder<QuranCubit,QuranState>(
+      builder:(context, state) => Expanded(
+        child: ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (_, index) => SuraItem(suraData: controller.surasSearchList[index]),
+            separatorBuilder: (context, index) => const SuraDivider(),
+            itemCount: controller.surasSearchList.length
+        ),
+      ),
+    );
+  }
+}
