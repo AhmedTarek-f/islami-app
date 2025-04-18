aa9534727c72ca48d1e8b0e626b5fdee1922294f
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-18 20:05:24 +0200

    using hadith page view instead of carousel view

--- lib/features/hadith/presentation/views/widgets/hadith_view_body.dart
+++ lib/features/hadith/presentation/views/widgets/hadith_view_body.dart
@@ -3,11 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:islami_app/core/common_widgets/islami_logo.dart';
 import 'package:islami_app/core/common_widgets/loading_indicator.dart';
-import 'package:islami_app/core/constants/app_data/app_data.dart';
-import 'package:islami_app/features/hadith/presentation/views/widgets/hadith_item.dart';
+import 'package:islami_app/features/hadith/presentation/views/widgets/hadith_page_view.dart';
 import 'package:islami_app/features/hadith/presentation/views_model/hadith_cubit.dart';
 import 'package:islami_app/features/hadith/presentation/views_model/hadith_state.dart';
-import 'package:islami_app/features/hadith_details/presentation/views/hadith_details_view.dart';
 
 class HadithViewBody extends StatelessWidget {
   const HadithViewBody({super.key});
@@ -18,26 +16,13 @@ class HadithViewBody extends StatelessWidget {
     return Column(
       children: [
         const IslamiLogo(),
-        SizedBox(height: 55.h,),
+        SizedBox(height: 40.h,),
         Expanded(
           child: BlocBuilder<HadithCubit,HadithState>(
             buildWhen: (previous, current) => current is LoadHadithSuccessState,
             builder: (context, state) =>controller.isLoading?
                 const LoadingIndicator()
-                :CarouselView.weighted(
-              controller: controller.carouselController,
-              elevation: 0,
-              scrollDirection: Axis.horizontal,
-              flexWeights: const [1,7,1],
-              itemSnapping: true,
-              onTap: (value) => Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) => HadithDetailsView(
-                    hadithData: AppData.hadithList[value]
-                ),
-                ),
-              ),
-              children: AppData.hadithList.map((hadith) => HadithItem(hadithData: hadith)).toList(),
-            ),
+                :const HadithPageView(),
           ),
         ),
         SizedBox(height: 20.h,),
