e4521483bf1f039c550f2d997a18a45555aa6f76
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-19 22:42:47 +0200

    changing quran view design to be better

--- lib/features/quran/presentation/views/widgets/quran_view_body.dart
+++ lib/features/quran/presentation/views/widgets/quran_view_body.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:islami_app/core/common_widgets/islami_logo.dart';
-import 'package:islami_app/core/constants/app_colors.dart';
-import 'package:islami_app/core/constants/app_fonts.dart';
-import 'package:islami_app/features/quran/presentation/views/widgets/recent_suras_list.dart';
-import 'package:islami_app/features/quran/presentation/views/widgets/sura_search_field.dart';
+import 'package:islami_app/features/quran/presentation/views/widgets/quran_sliver_app_bar.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/suras_list.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/suras_search_list.dart';
 import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
@@ -17,39 +12,15 @@ class QuranViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final controller = BlocProvider.of<QuranCubit>(context);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const IslamiLogo(),
-        SizedBox(height: 21.h,),
-        const SuraSearchField(),
-        SizedBox(height: 20.h,),
-        BlocBuilder<QuranCubit,QuranState>(
-            builder: (context, state) => controller.surasSearchList.isNotEmpty?
-                const SurasSearchList()
-                :Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Most Recently ",
-                    style: AppFonts.fontSize16Bold.copyWith(color: AppColors.offWhite),
-                  ),
-                  SizedBox(height: 10.h,),
-                  const RecentSurasList(),
-                  SizedBox(height: 10.h,),
-                  Text(
-                    "Suras List ",
-                    style: AppFonts.fontSize16Bold.copyWith(color: AppColors.offWhite),
-                  ),
-                  SizedBox(height: 10.h,),
-                  const SurasList(),
-                ],
-              ),
-            ),
-        )
-
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        const QuranSliverAppBar()
       ],
+      body: BlocBuilder<QuranCubit,QuranState>(
+        builder: (context, state) => controller.surasSearchList.isNotEmpty?
+        const SurasSearchList() :
+        const SurasList(),
+      ),
     );
   }
 }
