7a14711c91b50c9b64fec1af7b41d9e6bf8c8b2c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-19 22:45:16 +0200

    adding the sura list text inside a column

--- lib/features/quran/presentation/views/widgets/suras_list.dart
+++ lib/features/quran/presentation/views/widgets/suras_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/core/constants/app_colors.dart';
 import 'package:islami_app/core/constants/app_data/app_data.dart';
+import 'package:islami_app/core/constants/app_fonts.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/sura_divider.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/sura_item.dart';
 
@@ -8,13 +11,23 @@ class SurasList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          itemBuilder: (_, index) => SuraItem(suraData: AppData.surasList[index]),
-          separatorBuilder: (context, index) => const SuraDivider(),
-          itemCount: AppData.surasList.length
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Suras List ",
+          style: AppFonts.fontSize16Bold.copyWith(color: AppColors.offWhite),
+        ),
+        SizedBox(height: 10.h,),
+        Expanded(
+          child: ListView.separated(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (_, index) => SuraItem(suraData: AppData.surasList[index]),
+              separatorBuilder: (context, index) => const SuraDivider(),
+              itemCount: AppData.surasList.length
+          ),
+        ),
+      ],
     );
   }
 }
