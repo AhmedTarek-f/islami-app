11243d1a0bfb3dd99e550faf5e9951a138abd849
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-19 22:47:54 +0200

    adding bloc builder

--- lib/features/quran/presentation/views/widgets/recent_suras_list.dart
+++ lib/features/quran/presentation/views/widgets/recent_suras_list.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:islami_app/core/constants/app_data/app_data.dart';
 import 'package:islami_app/features/quran/presentation/views/widgets/recent_sura_item.dart';
 import 'package:islami_app/features/quran/presentation/views_model/quran_cubit.dart';
+import 'package:islami_app/features/quran/presentation/views_model/quran_state.dart';
 
 class RecentSurasList extends StatelessWidget {
   const RecentSurasList({super.key});
@@ -13,12 +13,15 @@ class RecentSurasList extends StatelessWidget {
     final controller = BlocProvider.of<QuranCubit>(context);
     return SizedBox(
       height: 150.h,
-      child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (_, index) => RecentSuraItem(suraData: AppData.surasList[index]),
-          separatorBuilder: (context, index) => SizedBox(width: 10.w,),
-          itemCount: AppData.surasList.length
+      child: BlocBuilder<QuranCubit,QuranState>(
+        buildWhen: (previous, current) => current is AddSuraToRecent,
+        builder:(context, state) => ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (_, index) => RecentSuraItem(suraData: controller.recentSurasList[index]),
+            separatorBuilder: (context, index) => SizedBox(width: 10.w,),
+            itemCount: controller.recentSurasList.length
+        ),
       ),
     );
   }
