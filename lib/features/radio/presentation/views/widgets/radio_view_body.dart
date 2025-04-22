4fa625550cf2b9425a32d5d01df47f0998e26561
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-22 16:50:51 +0200

    adding radio view design and logic

--- lib/features/radio/presentation/views/widgets/radio_view_body.dart
+++ lib/features/radio/presentation/views/widgets/radio_view_body.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:islami_app/features/radio/presentation/views/widgets/radio_channels_list.dart';
+import 'package:islami_app/features/radio/presentation/views/widgets/radio_sliver_app_bar.dart';
+import 'package:islami_app/features/radio/presentation/views/widgets/reciters_channels_list.dart';
+import 'package:islami_app/features/radio/presentation/views_model/radio_cubit.dart';
+import 'package:islami_app/features/radio/presentation/views_model/radio_state.dart';
 
 class RadioViewBody extends StatelessWidget {
   const RadioViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-
-      ],
+    final controller = BlocProvider.of<RadioCubit>(context);
+    return NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          const RadioSliverAppBar()
+        ],
+        body: RPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: BlocBuilder<RadioCubit,RadioState>(
+            buildWhen: (previous, current) => current is ToggleRadioOrRecitersState,
+            builder: (context, state) => controller.isRadio?
+            const RadioChannelsList():
+            const RecitersChannelsList(),
+          ),
+        )
     );
   }
 }
