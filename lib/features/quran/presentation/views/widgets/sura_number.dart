8a069d8563b0cc5f4c54cac210705f961d6f036c
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-23 15:51:30 +0200

    adding isBlackColor condition

--- lib/features/quran/presentation/views/widgets/sura_number.dart
+++ lib/features/quran/presentation/views/widgets/sura_number.dart
@@ -6,9 +6,10 @@ import 'package:islami_app/core/constants/app_images.dart';
 
 class SuraNumber extends StatelessWidget {
   const SuraNumber({
-    super.key, required this.suraNumber,
+    super.key, required this.suraNumber, this.isBlackColor = false,
   });
   final String suraNumber;
+  final bool isBlackColor;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -20,13 +21,14 @@ class SuraNumber extends StatelessWidget {
           Image.asset(
             AppImages.suraNumberImg,
             fit: BoxFit.cover,
+            color: isBlackColor? AppColors.black : AppColors.white,
           ),
           RPadding(
             padding: const EdgeInsets.symmetric(horizontal: 14),
             child: FittedBox(
               child: Text(
                 suraNumber,
-                style: AppFonts.fontSize20Bold.copyWith(color: AppColors.white),
+                style: AppFonts.fontSize20Bold.copyWith(color: isBlackColor ? AppColors.black :AppColors.white),
                 textAlign: TextAlign.center,
               ),
             ),
