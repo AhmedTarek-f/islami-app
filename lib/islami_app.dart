30ee3bfc143bea78ca382752cb28428c29e4e39d
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-23 15:58:15 +0200

    providing the onBoardingCubit at the islami app and remove it from main

--- lib/islami_app.dart
+++ lib/islami_app.dart
@@ -7,13 +7,14 @@ import 'package:islami_app/core/constants/app_theme.dart';
 import 'package:islami_app/features/navigation_menu/presentation/views/navigation_menu_view.dart';
 import 'package:islami_app/features/navigation_menu/presentation/views_model/menu_bottom_navigation_cubit.dart';
 import 'package:islami_app/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:islami_app/features/on_boarding/presentation/views_model/on_boarding_cubit.dart';
 
 class IslamiApp extends StatelessWidget {
   const IslamiApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final bool isHomeScreen = SharedPreferencesHelper.getBool(
+    final bool? isHomeScreen = SharedPreferencesHelper.getBool(
       key: AppConstants.isHomeScreenKey,
     );
     return ScreenUtilInit(
@@ -22,12 +23,15 @@ class IslamiApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Islami',
         theme: AppTheme.lightMode,
-        home: isHomeScreen
+        home: isHomeScreen != null
             ? BlocProvider<MenuBottomNavigationCubit>(
                 create: (context) => MenuBottomNavigationCubit(),
                 child: const NavigationMenuView(),
               )
-            : const OnBoardingView(),
+            : BlocProvider<OnBoardingCubit>(
+            create: (context) => OnBoardingCubit(),
+            child: const OnBoardingView(),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:islami_app/core/cache/shared_preferences_helper.dart';
-import 'package:islami_app/features/on_boarding/presentation/views_model/on_boarding_cubit.dart';
 import 'package:islami_app/islami_app.dart';
 
 void main() async {
@@ -17,6 +15,5 @@ void main() async {
   await SharedPreferencesHelper.init();
   FlutterNativeSplash.remove();
 
-  runApp(BlocProvider<OnBoardingCubit>(
-      create: (context) => OnBoardingCubit(), child: const IslamiApp()));
+  runApp(const IslamiApp());
 }
