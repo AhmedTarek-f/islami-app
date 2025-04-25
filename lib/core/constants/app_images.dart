8101f316f409bf201d55dfc2ed826b1f953163c0
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-25 08:05:48 +0300

    adding new images

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,28 +1,46 @@
-abstract final class AppImages{
+abstract final class AppImages {
   // Images
   static const String hadithBorderLeft = "assets/images/hadith_border_left.png";
-  static const String hadithBorderRight = "assets/images/hadith_border_right.png";
-  static const String mostRecentlySuraImage = "assets/images/most_recently_sura_image.png";
+  static const String hadithBorderRight =
+      "assets/images/hadith_border_right.png";
+  static const String mostRecentlySuraImage =
+      "assets/images/most_recently_sura_image.png";
   static const String onBoarding1 = "assets/images/on_boarding_1.png";
   static const String onBoarding2 = "assets/images/on_boarding_2.png";
   static const String onBoarding3 = "assets/images/on_boarding_3.png";
   static const String onBoarding4 = "assets/images/on_boarding_4.png";
   static const String onBoarding5 = "assets/images/on_boarding_5.png";
-  static const String quranScreenBackground = "assets/images/quran_screen_background.png";
-  static const String hadithScreenBackground = "assets/images/hadith_screen_background.png";
-  static const String radioActiveBackground = "assets/images/radio_active_background.png";
-  static const String radioInactiveBackground = "assets/images/radio_inactive_background.png";
-  static const String radioScreenBackground = "assets/images/radio_screen_background.png";
-  static const String sebhaScreenBackground = "assets/images/sebha_screen_background.png";
-  static const String suraDetailsBorderLeft = "assets/images/sura_details_border_left.png";
-  static const String suraDetailsBorderRight = "assets/images/sura_details_border_right.png";
-  static const String suraDetailsScreenFooterImage = "assets/images/sura_details_screen_footer_image.png";
+  static const String quranScreenBackground =
+      "assets/images/quran_screen_background.png";
+  static const String hadithScreenBackground =
+      "assets/images/hadith_screen_background.png";
+  static const String radioActiveBackground =
+      "assets/images/radio_active_background.png";
+  static const String radioInactiveBackground =
+      "assets/images/radio_inactive_background.png";
+  static const String radioScreenBackground =
+      "assets/images/radio_screen_background.png";
+  static const String sebhaScreenBackground =
+      "assets/images/sebha_screen_background.png";
+  static const String suraDetailsBorderLeft =
+      "assets/images/sura_details_border_left.png";
+  static const String suraDetailsBorderRight =
+      "assets/images/sura_details_border_right.png";
+  static const String suraDetailsScreenFooterImage =
+      "assets/images/sura_details_screen_footer_image.png";
   static const String suraNumberImg = "assets/images/sura_number_img.png";
-  static const String timeScreenBackground = "assets/images/time_screen_background.png";
-  static const String hadithCardBackgroundImage = "assets/images/hadith_card_background_image.png";
-  static const String hadithScreenFooterImage = "assets/images/hadith_screen_footer_image.png";
+  static const String timeScreenBackground =
+      "assets/images/time_screen_background.png";
+  static const String hadithCardBackgroundImage =
+      "assets/images/hadith_card_background_image.png";
+  static const String hadithScreenFooterImage =
+      "assets/images/hadith_screen_footer_image.png";
   static const String sebhaUpperPart = "assets/images/sebha_upper_part.png";
   static const String sebhaBody = "assets/images/sebha_body.png";
+  static const String timeBackgroundCard =
+      "assets/images/time_background_card.png";
+  static const String morningAzkar = "assets/images/morning_azkar.png";
+  static const String eveningAzkar = "assets/images/evening_azkar.png";
 
   // logos
   static const String islamiMosqueLogo = "assets/logos/islami_mosque_logo.png";
