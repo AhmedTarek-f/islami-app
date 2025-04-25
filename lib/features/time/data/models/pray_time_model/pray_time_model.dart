28432bdfe0eca61ad91a01a54a7a2aa7af9457de
AhmedTarek <o0ahmedtarek0o@gmail.com>
2025-04-25 07:49:08 +0300

    adding pray time model

---
+++ lib/features/time/data/models/pray_time_model/pray_time_model.dart
@@ -0,0 +1,10 @@
+class PrayTimeMode {
+  const PrayTimeMode({
+    required this.prayerName,
+    required this.prayerTime,
+    required this.dayTime,
+  });
+  final String prayerName;
+  final String prayerTime;
+  final String dayTime;
+}
